2781dca4c2d23dba433b72e8179d15047cf5f271
Christelle Sattler <pro.christelle.sattler@gmail.com>
2023-12-04 17:46:00 +0100

    Refactor command action Make a switch for action Change const nb_options to array to use in_array Prepare to change function schedule to use filter option

--- homescreen.js
+++ homescreen.js
@@ -5,34 +5,26 @@ import pkg from "@caporal/core"
 const { program } = pkg;
 import inquirer from "inquirer";
 import CruParser from "./CruParser.js";
+import fs from "fs";
 
 let user = new User("bubulle");
-const NB_OPTIONS = 2;
+const NB_OPTIONS = [1, 2];
+
+function checkNoUpdate() {
+    return false;
+}
 
 if (user.isConnected() && user.hasPermission()){
     program
         .command('start', 'Start the programme')
-        .action(({logger}) => {
-            logger.info("ORUS - OrgaRoomUniSealand");
-            logger.info("Bonjour, que voulez vous faire aujourd'hui ?\n" +
-                "1) Voir votre emploi du temps\n" +
-                "2) Voir la liste des salles\n")
-            inquirer
-                .prompt([{name: "action", type: "number", message: "Entrez le chiffre de l'action choisie : "}])
-                .then((answers) => {
-                    if (Number.isInteger(answers[0]) && [...Array(NB_OPTIONS).keys()].indexOf(answers[0] - 1) !== -1){
-                        logger.info("Vous avez choisi l'option : "+answers[0])
-                    }
-                })
-                .catch((error) => {
-                    logger.error(error.message)
-                });
-
-        })
+        .action(startProgram)
         .command('test', "Test votre jeu de données au format CRU")
         .argument('<directoryName>', "Nom du dossier à tester")
         .action(({args }) => {
             let parser = new CruParser();
+            if (fs.existsSync('schedule.json') && fs.existsSync('syncDate') && checkNoUpdate()){
+
+            }
             parser.parseDirectory(args.directoryName)
             //logger.info("--------------------------CLASSROOMS : -----------------------------");
             //logger.info(JSON.stringify(Array.from(parser.classrooms).sort(), null, 2));
@@ -46,3 +38,43 @@ if (user.isConnected() && user.hasPermission()){
 
     program.run();
 }
+
+function startProgram({logger}) {
+    logger.info("ORUS - OrgaRoomUniSealand");
+    logger.info("Bonjour, que voulez vous faire aujourd'hui ?\n" +
+        "1) Voir votre emploi du temps\n" +
+        "2) Voir la liste des salles\n")
+    inquirer
+        .prompt([{name: "action", type: "number", message: "Entrez le chiffre de l'action choisie : "}])
+        .then((answers) => switchActions(logger, answers))
+        .catch((error) => {
+            logger.error(error.message)
+        });
+}
+
+function switchActions(logger, answers) {
+    if (Number.isInteger(answers.action) && NB_OPTIONS.find(v => answers.action === v)){
+        logger.info("Vous avez choisi l'option : "+answers.action)
+    }
+    switch (answers.action) {
+        case 1:
+            visualiseSchedule();
+            break;
+        case 2:
+            break;
+    }
+}
+
+
+function visualiseSchedule() {
+    inquirer
+        .prompt({message: "Entrez vos matières séparer par une virgule : ", type: "string", name: "courses"}, () => {})
+        .then((answers) => {
+            let tokenUes = answers.courses.split(/ , |, | ,|,|/);
+            console.log(tokenUes);
+            console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAH");
+        })
+
+}
+
+
