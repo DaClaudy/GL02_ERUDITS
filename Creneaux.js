d485d5e9e86cfc1c0c01a9da5759cf66af317c02
claudyDev <boundjadamigou54@gmail.com>
2023-12-08 23:30:16 +0000

    Ajout du constructeur et des attributs

--- Creneaux.js
+++ Creneaux.js
@@ -1,3 +1,10 @@
-class Creneau {
-    
+class Creneaux {
+    constructor(nomUe, capacite, jour, heureDebut, heureFin, salle) {
+        this.nomUe = nomUe;
+        this.capacite = capacite;
+        this.jour = jour;
+        this.heureDebut = heureDebut;
+        this.heureFin = heureFin;
+        this.salle = salle;
+    }
 }
