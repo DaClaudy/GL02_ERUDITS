4c310764eb11eb54283c1d3c390de840b93fb492
Christelle Sattler <pro.christelle.sattler@gmail.com>
2023-12-09 04:18:22 +0100

    Feat: Add build function to builder

--- CruBuilder.js
+++ CruBuilder.js
@@ -2,8 +2,27 @@ const CruBuilder = function() {
     this.name = '';
 }
 
-CruBuilder.prototype.unparse = function () {
-    return 0;
+CruBuilder.prototype.build = function (cruParser, ues) {
+    let time = Date.now();
+    let result = "SCHEDULE.CRU - Emploi du temps\r\n";
+    result += "Emploi du temps avec différentes salles au format cru"
+    result += "+UVUV"
+    result += cruParser.schedule.exemple;
+    let schedule = cruParser.schedule.ues.filter(v => ues.includes(this.name))
+    for (let ues of schedule) {
+        result += this.ue();
+    }
+    time = (new Date(time.getTime())) - (new Date(Date.now())).getTime();
+    result += "Page générée en :" + time +" sec";
+    return true;
+}
+
+CruBuilder.prototype.ue = function () {
+    return '';
+}
+
+CruBuilder.prototype.course = function (){
+    return '';
 }
 
 export default CruBuilder;
